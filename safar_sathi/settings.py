7d529bae27f5338f8fef2611bf8ca057bfd82b0c
Md.Mohibur Rahman <23101006@uap-bd.edu>
2025-10-19 01:38:07 +0600

    change in setting.py
    Added email backend configuration for console output.

--- safar_sathi/settings.py
+++ safar_sathi/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'safar_sathi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -142,6 +142,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 LOGIN_URL = 'accounts:login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
