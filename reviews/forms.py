35236edda0efe62c1f6625091da8f6bc76833320
Md.Mohibur Rahman <23101006@uap-bd.edu>
2025-10-20 01:15:50 +0600

    Update forms.py

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,23 +1,32 @@
 from django import forms
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Field, Submit, Div, HTML
-from .models import DestinationReview, AccommodationReview, ReviewPhoto
+from crispy_forms.layout import Layout, Field, Submit
+from .models import DestinationReview, AccommodationReview, GuideReview, ReviewPhoto
 
 
 class DestinationReviewForm(forms.ModelForm):
     class Meta:
         model = DestinationReview
-        fields = ['title', 'content', 'rating']
+        fields = ['content', 'rating']
         widgets = {
-            'content': forms.Textarea(attrs={'rows': 4}),
-            'rating': forms.Select(attrs={'class': 'form-select'}),
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Share your experience about this destination...'
+            }),
+            'rating': forms.Select(attrs={
+                'class': 'form-select'
+            }),
+        }
+        labels = {
+            'content': 'Your Review',
+            'rating': 'Rating'
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
-            Field('title', css_class='form-control'),
             Field('rating', css_class='form-select'),
             Field('content', css_class='form-control'),
             Submit('submit', 'Submit Review', css_class='btn btn-primary')
@@ -27,17 +36,55 @@ class DestinationReviewForm(forms.ModelForm):
 class AccommodationReviewForm(forms.ModelForm):
     class Meta:
         model = AccommodationReview
-        fields = ['title', 'content', 'rating']
+        fields = ['content', 'rating']
         widgets = {
-            'content': forms.Textarea(attrs={'rows': 4}),
-            'rating': forms.Select(attrs={'class': 'form-select'}),
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Share your experience about this accommodation...'
+            }),
+            'rating': forms.Select(attrs={
+                'class': 'form-select'
+            }),
+        }
+        labels = {
+            'content': 'Your Review',
+            'rating': 'Rating'
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
-            Field('title', css_class='form-control'),
+            Field('rating', css_class='form-select'),
+            Field('content', css_class='form-control'),
+            Submit('submit', 'Submit Review', css_class='btn btn-primary')
+        )
+
+
+class GuideReviewForm(forms.ModelForm):
+    class Meta:
+        model = GuideReview
+        fields = ['content', 'rating']
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Share your experience with this guide...'
+            }),
+            'rating': forms.Select(attrs={
+                'class': 'form-select'
+            }),
+        }
+        labels = {
+            'content': 'Your Review',
+            'rating': 'Rating'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
             Field('rating', css_class='form-select'),
             Field('content', css_class='form-control'),
             Submit('submit', 'Submit Review', css_class='btn btn-primary')
@@ -49,25 +96,9 @@ class ReviewPhotoForm(forms.ModelForm):
         model = ReviewPhoto
         fields = ['image', 'caption']
         widgets = {
-            'image': forms.FileInput(attrs={'class': 'form-control', 'accept': 'image/*'}),
-            'caption': forms.TextInput(attrs={'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
+            'caption': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Optional caption for the photo'
+            })
         }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.layout = Layout(
-            Field('image', css_class='form-control'),
-            Field('caption', css_class='form-control'),
-            Submit('submit', 'Upload Photo', css_class='btn btn-primary')
-        )
-
-
-# Multiple photo upload formset
-ReviewPhotoFormSet = forms.modelformset_factory(
-    ReviewPhoto,
-    form=ReviewPhotoForm,
-    extra=3,
-    max_num=5,
-    can_delete=True
-)
