951843f27bfa94ced06e7585a7dad99e94f8363a
Jayan Tasnim <158599727+JTI22@users.noreply.github.com>
2025-10-22 08:45:40 +0600

    Update admin.py

--- destinations/admin.py
+++ destinations/admin.py
@@ -1,33 +1,17 @@
 from django.contrib import admin
-from .models import Destination, Photo, DestinationFeature
+from .models import Destination, Photo
 
 # Register your models here.
 @admin.register(Destination)
 class DestinationAdmin(admin.ModelAdmin):
-    list_display = ['name', 'category', 'location', 'state', 'is_featured', 'created_at']
-    list_filter = ['category', 'state', 'is_featured', 'created_at']
-    search_fields = ['name', 'location', 'state', 'description']
-    actions = ['make_featured', 'remove_featured']
+    list_display = ['name', 'category', 'location',  'created_at']
+    list_filter = ['category',  'created_at']
+    search_fields = ['name', 'location', 'description']
 
-    def make_featured(self, request, queryset):
-        queryset.update(is_featured=True)
-
-    make_featured.short_description = "Mark selected destinations as featured"
-
-    def remove_featured(self, request, queryset):
-        queryset.update(is_featured=False)
-
-    remove_featured.short_description = "Remove featured status"
 
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ['destination', 'caption', 'uploaded_by', 'is_primary', 'uploaded_at']
-    list_filter = ['is_primary', 'uploaded_at', 'destination']
+    list_display = ['destination', 'caption', 'uploaded_by', 'uploaded_at']
+    list_filter = [ 'uploaded_at', 'destination']
     search_fields = ['destination__name', 'caption']
-
-
-@admin.register(DestinationFeature)
-class DestinationFeatureAdmin(admin.ModelAdmin):
-    list_display = ['destination', 'feature_name']
-    list_filter = ['destination']
