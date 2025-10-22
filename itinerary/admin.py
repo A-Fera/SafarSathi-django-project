15506ddf3fef461f2ee2eb2550600a8b34d5902d
Samiul Islam <samiulrakib8822@gmail.com>
2025-10-22 09:38:48 +0600

    Update admin.py

--- itinerary/admin.py
+++ itinerary/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Itinerary, ItineraryItem, ItineraryCollaborator
+from .models import Itinerary, ItineraryItem
 
 class ItineraryItemInline(admin.TabularInline):
     model = ItineraryItem
